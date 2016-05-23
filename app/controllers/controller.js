20502c17f4e05fbec19cb6962092289006ab4307
ltdelia <delia.louis@gmail.com>
2016-05-22 22:33:55 -0400

    created initial bootstrap layout, index, and student form page

---
+++ app/controllers/controller.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+var path = require('path');
+
+
+module.exports = function(app){
+
+	app.get('/', function(req, res){
+		res.sendFile(path.join(__dirname+'/../public/index.html'));
+	});
+
+	app.get('/form', function(req, res){
+		res.sendFile(path.join(__dirname+'/../public/studentForm.html'));
+	});
+}
